170529c09226c3654da4339d912b64688cc3a1f8
Nasser <80926182+nasseralm@users.noreply.github.com>
2021-07-13 13:15:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const query = document.querySelector.bind(document);
+const queryAll = document.querySelectorAll.bind(document);
+/// Uso do bind conforme:
+/// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Function/bind
+/// https://stackoverflow.com/questions/2236747/what-is-the-use-of-the-javascript-bind-method
+
+const taskSelector = (task) =>
+  task.addEventListener('click', (event) => {
+    if (query('.selected')) query('.selected').classList.remove('selected');
+    event.target.classList.add('selected');
+  });
+
+const marker = (element) => {
+  element.addEventListener('dblclick', (event) => {
+    event.target.classList.toggle('completed');
+  });
+};
+
+/// Uso de elements, arrow functions conforme:
+/// https://www.w3schools.com/jsref/dom_obj_all.asp
+/// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+/// https://stackoverflow.com/questions/47591258/how-to-use-this-in-an-arrow-function-for-referencing-the-clicked-element
+
+const addTaskButton = () => {
+  const taskItem = document.createElement('li');
+  query('#lista-tarefas').appendChild(taskItem).classList.add('item');
+  taskSelector(taskItem);
+  marker(taskItem);
+  taskItem.innerText = query('#texto-tarefa').value;
+  query('#texto-tarefa').value = '';
+};
+
+query('#criar-tarefa').addEventListener('click', addTaskButton);
+
+query('#apaga-tudo').addEventListener('click', () =>
+  queryAll('.item').forEach((element) => element.remove()));
+
+query('#remover-finalizados').addEventListener('click', () => {
+  queryAll('.completed').forEach((element) => element.remove());
+});
+
+query('#remover-selecionado').addEventListener('click', () =>
+  queryAll('.selected').forEach((element) => element.remove()));
+
+/// forEach, conforme:
+/// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+/// https://blog.betrybe.com/javascript/javascript-foreach/
+
+// Array.prototype.move = (element, offset) => {
+//   const index = this.indexOf(element)
+//   const newIndex = index + offset
+
+//   if newIndex > -1 && newIndex < this.length
+//     // # Remove the element from the array
+//     const removedElement = this.splice(index, 1)[0]
+
+//     // # At "newIndex", remove 0 elements, insert the removed element
+//     this.splice(newIndex, 0, removedElement)
+// }
+let list = query('#lista-tarefas')
+
+query('#up').addEventListener('click', () =>
+  list.move(queryAll('.selected'), -1));
